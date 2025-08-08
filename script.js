750fe35fc0c435ef07f78980c3dd5cbf56a127ad
Jusxyg <narantigua@gmail.com>
2025-08-08 14:39:59 +0800

    changes to main

--- script.js
+++ script.js
@@ -42,13 +42,17 @@ document.addEventListener('DOMContentLoaded', () => {
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
+        // Update each span individually
         document.getElementById('days').textContent = days;
         document.getElementById('hours').textContent = hours;
         document.getElementById('minutes').textContent = minutes;
         document.getElementById('seconds').textContent = seconds;
     }
 
-    updateCountdown(); // Initial call
+
+    updateCountdown();
+
+
     setInterval(updateCountdown, 1000);
 
     
